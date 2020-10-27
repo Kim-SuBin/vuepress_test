5ca0aa3ee7234ec18c8adc4ac58078363533895f
Kim-SuBin <sb2504@naver.com>
2020-10-27 23:51:50 +0900

    [TEST] sidebar
    조금 귀찮아도 이런 형태로 sidebar를 써야 dev와 build가 정상적으로 이루어짐

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,9 @@
 module.exports = {
-    title: 'VuePress 시작!',
-    description: 'Hello World',
+    title: 'Python Mentoring',
+    description: 'HUFS RC SW Mentoring Program 강의 자료',
     head: [
         ['link', { rel: 'icon', href: '/images/logo-144.png' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: `/images/logo-144.png` }],
-        ['link', { rel: 'mask-icon', href: '/images/logo-144.png', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/images/logo-144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+        ['link', { rel: 'manifest', href: '/manifest.json' }]
     ],
     plugins: [
         '@vuepress/back-to-top',
@@ -26,14 +19,11 @@ module.exports = {
             { text: 'Home', link: '/' },
             { text: 'About', link: '/about/' }
         ],
-        sidebar: {
-            '/about/': [
-                ''
-            ],
-            '/': [
-                '',
-                'foo-nav'
-            ]
-        }
+        sidebar: [
+            '/',
+            'foo-nav',
+            '/about/',
+            '/about/testpage'
+        ]
     }
 }
