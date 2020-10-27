8586285342c0dd38909d8adc34e27ff2ea27ade9
Kim-SuBin <sb2504@naver.com>
2020-10-28 03:04:22 +0900

    [Commit] 내가 뭘 하는지 모르겠다..

---
+++ docs/.vuepress/components/Home.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="titleContainer">
+      <h1>
+        Python
+        <br>Mentoring
+      </h1>
+      <div class="infoContainer">
+        <div class="name">Subin Kim</div>
+        <ul>
+          <li>
+            <a href="https://subin-0320.tistory.com/">Blog</a>
+          </li>
+          <li>
+            <a href="https://github.com/Kim-SuBin">GitHub</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <br/>
+    <div class="footer">
+      <hr/>
+      <p class="copyright">
+       © 2020 SuBin Kim
+       <br/>
+       This site/repository is licensed under the MIT license. See <a href="https://github.com/Kim-SuBin/python_mentoring/blob/master/LICENSE">LICENSE</a> for details.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@import "https://fonts.googleapis.com/css?family=Gugi";
+.infoContainer {
+  text-align: right;
+}
+h1 {
+  font-family: "Gugi", cursive;
+  font-weight: 500;
+  border-bottom: 17px solid #16a2ff;
+  word-break: keep-all;
+  font-size: 5rem;
+}
+.name {
+  font-weight: 900;
+  font-size: 2rem;
+}
+ul {
+  list-style: none;
+  line-height: 1.5rem;
+}
+@media (max-width: 550px) {
+  h1 {
+    font-size: 4.4rem;
+  }
+  .name {
+    font-size: 1.7rem;
+  }
+}
+@media (max-width: 430px) {
+  h1 {
+    font-size: 3.4rem;
+  }
+}
+.footer{
+  text-align: center;
+}
+.copyright {
+  color: gray;
+  display: inline-block;
+}
+</style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'Python Mentoring',
     description: 'HUFS RC SW Mentoring Program 강의 자료',
     email: '2504sb@gmail.com',
-    base: 'python_mentoring',
+    // base: 'python_mentoring',
     head: [
         ['link', { rel: 'icon', href: '/images/logo-144.png' }],
         ['link', { rel: 'manifest', href: '/manifest.json' }]
