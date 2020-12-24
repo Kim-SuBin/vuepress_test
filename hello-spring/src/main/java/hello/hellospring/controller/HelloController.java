d1ff03a2f637034eea3206498fbc38932cec35fc
Kim-SuBin <sb2504@naver.com>
2020-12-25 05:18:19 +0900

    [Update] ✨ HelloController.java
    MVC 확인을 위한 Controller 추가

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 // Controller 사용 시 @Controller라고 Annotation을 추가해야 함
 @Controller
@@ -13,5 +14,12 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    // MVC를 위해 추가
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
