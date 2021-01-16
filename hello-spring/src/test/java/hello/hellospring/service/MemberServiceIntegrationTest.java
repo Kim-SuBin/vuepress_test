f71ce06236792bfcee31f34219b97729de560f14
Kim-SuBin <sb2504@naver.com>
2021-01-16 21:40:52 +0900

    [Add] ✨ MemberServiceIntegrationTest.java - 스프링통합테스트
    스프링컨테이너와 DB를 연결하여 통합테스트를 진행하는 코드

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,49 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest // 스프링 컨테이너와 테스트를 함께 실행
+@Transactional // 테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고, 테스트 완료 후 항상 롤백 => DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지 않음
+class MemberServiceIntegrationTest {
+
+    // 테스트의 경우 가장 편한 방법으로 진행하는 것이 더 좋으므로 바로 @Autowired 하는 것이 나음
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        // given
+        Member member = new Member();
+        member.setName("spring");
+        // when
+        Long saveId = memberService.join(member);
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복회원예외(){
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+
+}
