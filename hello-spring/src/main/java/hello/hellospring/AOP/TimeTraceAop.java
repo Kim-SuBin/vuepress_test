8b734003c049b6c0a9f8e49a6529152509936f5d
Kim-SuBin <sb2504@naver.com>
2021-01-17 19:12:17 +0900

    [Add] ✨ TimeTraceAop.java
    AOP를 사용하여 시간 측정하는 형태

---
+++ hello-spring/src/main/java/hello/hellospring/AOP/TimeTraceAop.java
@@ -0,0 +1,23 @@
+package hello.hellospring.AOP;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect // AOP로 사용하기 위해서는 @Aspect 어노테이션을 써야한다.
+public class TimeTraceAop {
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed(); // 다음 메서드로 진행
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString()+ " " + timeMs + "ms");
+        }
+    }
+}
