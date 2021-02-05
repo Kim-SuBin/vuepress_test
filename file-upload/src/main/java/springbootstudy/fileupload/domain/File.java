08bba4294e44a3a46cce31c602b8e5135fbb753c
Kim-SuBin <sb2504@naver.com>
2021-02-06 02:36:38 +0900

    [Add] ✨ File Domain (Entity)

---
+++ file-upload/src/main/java/springbootstudy/fileupload/domain/File.java
@@ -0,0 +1,31 @@
+package springbootstudy.fileupload.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "file")
+public class File {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String filePath;
+
+    @Builder
+    public File(Long id, String title, String filePath) {
+        this.id = id;
+        this.title = title;
+        this.filePath = filePath;
+    }
+}
